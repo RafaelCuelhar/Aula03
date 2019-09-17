d3397827c39a76363c57b8bbe4bbfa763f098f1b
arqdsis <arqdsis@BUTLI0236.animaedu.intranet>
2019-09-16 21:38:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/aula002_02/Localizacao.java
@@ -0,0 +1,30 @@
+package br.com.aula002_02;
+
+import android.widget.ListView;
+
+class Localizacao {
+
+    private Double latitude;
+    private Double longitude;
+
+    public Localizacao(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
